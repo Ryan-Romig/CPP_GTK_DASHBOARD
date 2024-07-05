e048728659e7e3c4668b08afcd5cb9ce1c199359
Ryan <rromig92@gmail.com>
2024-07-05 12:48:50 -0700

    added css to custom widget

--- src/components/buttons/launcher_button/launcher_button.c
+++ src/components/buttons/launcher_button/launcher_button.c
@@ -10,7 +10,14 @@ static void launcher_button_class_init(LauncherButtonClass* klass)
     GtkWidgetClass* widget_class = GTK_WIDGET_CLASS(klass);
 
     gtk_widget_class_set_template_from_resource(widget_class, "/com/melodia/launcher/ui/components/launcher_button.ui");
-
-
 }
-static void launcher_button_init(LauncherButton* self) { gtk_widget_init_template(GTK_WIDGET(self)); }
+static void launcher_button_init(LauncherButton* self)
+{
+
+    gtk_widget_init_template(GTK_WIDGET(self));
+     GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_resource(provider, "/com/melodia/launcher/ui/styles/launcher_button.css");
+    gtk_style_context_add_provider_for_display(gdk_display_get_default(),
+                                               GTK_STYLE_PROVIDER(provider),
+                                               GTK_STYLE_PROVIDER_PRIORITY_USER);
+}
